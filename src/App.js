1759cfcc7d0d367fc211528eaa5e3580a9748d33
Amisha Singh <singhamisha.871@gmail.com>
2021-07-06 21:38:00 +0530

    My Message and Their Mesage updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import { ChatEngine} from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <ChatEngine 
+           height="100vh"
+           projectID="72026c6f-f660-43e7-b2a2-a3a022cb32a3"
+           userName="singhamisha"
+           userSecret="Amisha123*"
+           renderChatFeed={(chatAppProps)=> <ChatFeed  {...chatAppProps}/>}
+        />
+    );
 }
-
 export default App;
---
+++ src/components/MessageForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const MessageForm = () => {
+    return (
+        <div>
+            MessageForm
+        </div>
+    );
+
+}
+export default MessageForm;
